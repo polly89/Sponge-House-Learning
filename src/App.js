d9856909f7331d86d09e4da6ca917eaf608054b1
Christina Woodhams <cwoodhams89@gmail.com>
2023-04-28 13:20:34 -0400

    Eight commit. Sending background and audio to the game

--- src/App.js
+++ src/App.js
@@ -11,14 +11,16 @@ import CreateGame from './pages/CreateGame';
 
 function App (){
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route index element ={<Home />}/>
-                <Route exact path='/auth' element={<Auth/>}/>
-                <Route exact path='/createGame' element={<CreateGame/>}/>
-                <Route exact path='/game/:id' element={<Game/>}/>
-            </Routes>
-        </BrowserRouter>
+        
+            <BrowserRouter>
+                <Routes>
+                    <Route index element ={<Home />}/>
+                    <Route exact path='/auth' element={<Auth/>}/>
+                    <Route exact path='/createGame' element={<CreateGame/>}/>
+                    <Route exact path='/game/:id' element={<Game/>}/>
+                </Routes>
+            </BrowserRouter>
+        
     );
 }
 export default App;
--- src/pages/Game.js
+++ src/pages/Game.js
@@ -2,15 +2,21 @@ import './Game.css'
 import { Link, useParams } from 'react-router-dom';
 import {useState, useEffect } from 'react';
 import { db } from '../config/firebase';
-import { doc, getDoc } from 'firebase/firestore'
+import { doc, getDoc } from 'firebase/firestore';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import FeedThePig from '../components/FeedThePig';
+import BuildAHouse from '../components/BuildAHouse';
+import PickApples from '../components/PickApples'
 
 function Game(){
     const { id } = useParams()
     const gameId = id.replace(':', '')
     const [game, setGame] = useState([]); 
+    const [backdrop, setBackdrop] = useState('')
+    const [audio, setAudio] = useState('')
 
     const gameRef = doc(db, 'games', `${gameId}`)
-    console.log({id})
     console.log(gameId)
     
     useEffect(()=> {
@@ -19,6 +25,8 @@ function Game(){
             const docSnap = await getDoc(gameRef);
             setGame(docSnap.data())
             console.log('Game data:', docSnap.data())
+            setBackdrop(docSnap.data().Backdrop)
+            setAudio(docSnap.data().Audio)
         } catch(err){
             console.log(err)
         }
@@ -28,6 +36,7 @@ function Game(){
     }, [])
 
     return (
+        <DndProvider backend={HTML5Backend}>
         <div className='page-styling'>
             <img className='game-circles'src='https://myawsbucketmundoimages.s3.us-east-2.amazonaws.com/Group+17.png' alt='stylized shaples, blue and yellow'/>
             <div className='sub-head'>
@@ -40,13 +49,18 @@ function Game(){
             </div>
             <div className='game-border'>
                 <img className='game-star-1'src='https://myawsbucketmundoimages.s3.us-east-2.amazonaws.com/Star+4.png' alt='star'/>
-                <div className='game-cont' key={gameId} style={{backgroundImage: `url(${game.Backdrop})`,backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
-                    <div  ></div>
+                <div  key={gameId}>
+                    <img className='game-cont' src={backdrop} alt='backdrop'/>
+                    <div onLoad={(e) => (audio.play())}className='audio-element'>
+                        <audio controls src={audio} className='absolute z-10 mt-4 ml-28 rotate-2 rounded-full hover:bg-sky-200 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300'/>
+                    </div>
+                   {/* Replace this with an if statement so it displays components based on the game type or image url */}
+                    <BuildAHouse />
                 <img className='game-star-2'src='https://myawsbucketmundoimages.s3.us-east-2.amazonaws.com/Star+4.png' alt='star'/>
                 </div> 
             </div>
-            
          </div>
+         </DndProvider>
     )
 };
 export default Game;
