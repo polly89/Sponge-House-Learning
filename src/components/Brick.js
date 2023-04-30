3c32ae8a58aa31d7328a226125f844ef69c4f477
Christina Woodhams <cwoodhams89@gmail.com>
2023-04-30 10:09:52 -0400

    Eleventh commit. Fixed re-render and randomizer. New bug: timing error, the text doesn't always display once it's dragged.

--- src/components/Brick.js
+++ src/components/Brick.js
@@ -1,13 +1,6 @@
 import { useDrag } from 'react-dnd';
 
-function Brick({id, url, answer}){
-    const answerScramble = answer.split('').sort(function(){return 0.5- Math.random()})
-    console.log(answerScramble)
-    let i = 0
-    // ISSUES: 
-    // App is rerendering and generating a random letter on every drag/drop need useEffect
-    // Letters are showing duplicates
-    // DND library doesn't cary the h1 element into the dropZone... can I add it as an acceptable type? Or should I replace my images with a rectangular div?
+function Brick({id, url, answer}){  
 
     const [{isDragging}, drag] = useDrag(()=> ({
         type:'brick', 
@@ -21,7 +14,7 @@ function Brick({id, url, answer}){
         ref={drag} 
         style={{border: isDragging ? '5px solid pink' : "0px"}}>
             <img src={url} alt='brick' className=''/>
-            <h1 className='-mt-14 text-center gap-4'>{answerScramble[i++]}</h1> 
+            <h1 className='-mt-14 text-center gap-4'>{answer}</h1> 
         </div>
     );
 }
--- src/components/BuildAHouse.js
+++ src/components/BuildAHouse.js
@@ -1,9 +1,11 @@
 import Brick from "./Brick"
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useDrop } from 'react-dnd';
 
 function BuildAHouse({ answer }){
     const [board, setBoard] = useState([]);
+    const [options, setOptions] = useState([]);
+
     const [{isOver}, drop] = useDrop(()=> ({
         accept:'brick',
         drop: (item)=> addBrickToHouse(item.id),
@@ -11,33 +13,44 @@ function BuildAHouse({ answer }){
             isOver: !!monitor.isOver(),
         }),
     }));
+    const arr = answer.split('')
+    console.log(arr)
+    
+    // useEffect(()=> {
+    //     setOptions(arr)
+    // }, [])
 
-    const addBrickToHouse =(id)=> {
+
+    const addBrickToHouse = (id)=> {
         const stackedBricks = bricks.filter((brick)=> id === brick.id);
         setBoard((board)=> [...board, stackedBricks[0]]);
+
     }
     const bricks = [
         {
             id: 1,
-            url: 'https://myawsbucketmundoimages.s3.us-east-2.amazonaws.com/Rectangle+40.png'
+            url: 'https://myawsbucketmundoimages.s3.us-east-2.amazonaws.com/Rectangle+40.png',
+            text: arr[2]
         },
         {
             id: 2,
-            url: 'https://myawsbucketmundoimages.s3.us-east-2.amazonaws.com/Rectangle+40.png'
+            url: 'https://myawsbucketmundoimages.s3.us-east-2.amazonaws.com/Rectangle+40.png',
+            text: arr[0]
         },
         {
             id: 3,
-            url: 'https://myawsbucketmundoimages.s3.us-east-2.amazonaws.com/Rectangle+40.png'
+            url: 'https://myawsbucketmundoimages.s3.us-east-2.amazonaws.com/Rectangle+40.png',
+            text: arr[1]
         }
     ]
     return (
         <>
         <div className='flex justify-center gap-4 -mt-48 z-10 absolute rotate-2'>{bricks.map((brick)=>{
-            return <Brick url={brick.url} id={brick.id} answer={answer}/>
+            return <Brick url={brick.url} id={brick.id} answer={brick.text}/>
         })}</div>
         <div className='w-80 h-2/6 absolute mt-16 ml-96 rotate-2 justify-center space-y-6' ref={drop}>
             {board.map((brick)=> {
-                return<Brick url={brick.url} id={brick.id}/>
+                return<Brick url={brick.url} id={brick.id} answer={brick.text}/>
             })}
         </div>
         </>
